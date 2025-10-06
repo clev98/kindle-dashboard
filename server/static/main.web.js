e5f205217d93bcef3eb808e58e16ef92836c48e3
Kit <connorleavesley1@gmail.com>
2025-10-05 20:10:09 -0400

    Moving back to archaic js :c

--- server/static/main.web.js
+++ server/static/main.web.js
@@ -1,14 +1,24 @@
+function onResponse() {
+    document.body.innerHTML = this.responseText;
+}
+
+// This would work if I had modern JS :(
+/*
 async function buttonClick() {
     const url = "http://192.168.1.11:8080/dashboard/";
-    const response = await fetch(url);
-
-    if (!response.ok) {
-        return;
-    }
-
-    const result = await response.text();
+    const result = await fetch(url)
+        .then(response => response.text())
+        .catch(error => { return });
 
     document.body.innerHTML = result;
+}
+*/
+
+function buttonClick() {
+    var req = new XMLHttpRequest();
+    req.addEventListener("load", onResponse);
+    req.open("GET", "http://192.168.1.11:8080/dashboard/");
+    req.send();
 }
 
 if(typeof intId === 'undefined') {
